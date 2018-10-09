72987c92f4db8c5a4797c75b8b5673b5c38855f2
jklmnn <jklmnn@web.de>
2018-10-09 10:07:42 +0200

    run one test per entry

--- data/test.py
+++ data/test.py
@@ -3,6 +3,7 @@
 import unittest
 import json
 import requests
+import ddt
 from jsonschema import validate
 
 indexschema = {
@@ -51,45 +52,39 @@ subschema = {
                         }
                 }
 
+def load_files():
+        jf = open("index.json", "r")
+        files = [_id['id'] for _id in json.load(jf)]
+        jf.close()
+        return files
+
+def load_links(files):
+        entries = []
+        for sfile in files:
+                with open("{0}.json".format(sfile)) as f:
+                        data = json.load(f)
+                        for semester in data:
+                                entries.extend(semester["entries"])
+        return entries
+
+@ddt.ddt
 class JsonValidate(unittest.TestCase):
 
-        def setUp(self):
-                jf = open("index.json", "r")
-                self.files = [_id['id'] for _id in json.load(jf)]
-                jf.close()
-
         def test_index(self):
-                self.validate_file("index")
+                with open("index.json", "r") as f:
+                        data = json.load(f)
+                validate(data, indexschema)
 
-        def test_files(self):
-                assert(self.files)
-                for _file in self.files:
-                        self.validate_file(_file)
+        @ddt.data(*load_files())
+        def test_files(self, value):
+                with open("{0}.json".format(value), "r") as f:
+                        data = json.load(f)
+                validate(data, subschema)
 
-        def test_links(self):
-                assert(self.files)
-                for _file in self.files:
-                        self.validate_links(_file)
+        @ddt.data(*load_links(load_files()))
+        def test_links(self, entry):
+                response = requests.head(entry["url"], verify=False)
+                self.assertTrue(response,
+                        "Test failed for\nSite: {0} \nURL: {1}\nStatus: {2}".format(
+                            entry["name"], entry["url"], response.status_code))
 
-
-        def validate_links(self, subject):
-                with open("{0}.json".format(subject), "r") as f:
-                    data = json.load(f)
-                    for semester in data:
-                            for entry in semester["entries"]:
-                                    self.assertTrue(requests.head(entry["url"], verify=False))
-
-        def validate_file(self, subject):
-                f = open("{0}.json".format(subject), "r")
-                raw = f.read()
-                f.close()
-                def test(self):
-                        try:
-                          data = json.loads(raw)
-                        except json.decoder.JSONDecodeError as e:
-                          raise AttributeError("Error in {0}: {1}".format(subject, str(e)))
-                        if subject == "index":
-                                validate(data, indexschema)
-                        else:
-                                validate(data, subschema)
-                return test
